be68cd8c1c1aa1349345399060ae995041d246cc
Svetlana Burlakova <svetlya4ok83@gmail.com>
2023-02-02 12:54:43 +0400

    Tak19

---
+++ Task19/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 10000 && result <100000)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введите 5-значное число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+bool Palindrom(int num)
+{
+    if ((num / 10000 == num%10) && ((num /1000)%10 == (num%100)/10)) return true;
+    else return false;
+}
+
+int number=GetNumber("Введите 5-значное число:");
+bool result=Palindrom(number);
+if (result==true) Console.WriteLine($"число {number} является палиндромом");
+else Console.WriteLine($"число {number} не является палиндромом");
+//Console.WriteLine(number/10000);
+//Console.WriteLine(number%10);
+//Console.WriteLine((number/1000)%10);
+//Console.WriteLine((number%100)/10);
